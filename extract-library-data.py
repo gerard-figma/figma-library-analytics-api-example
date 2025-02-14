049443879515d4622d098e933a0888ad5a29876c
Gerard White <gerardwhite@icloud.com>
2025-02-14 15:11:00 +0000

    adding variable support

--- extract-library-data.py
+++ extract-library-data.py
@@ -15,17 +15,18 @@ base_url = 'https://api.figma.com/v1/analytics/libraries/'
 
 # Set the dates 
 # TODO: Let's default to today, today-365
-start_date = '2024-01-01'
-end_date = '2024-04-05'
+start_date = '2024-09-01'
+end_date = '2025-04-05'
 
 
-# Actions by Component
+# COMPONENTS
 def actions_by_component():
-    params = "/actions?group_by=component&start_date=" + start_date + "&end_date=" + end_date + "&order=asc"
+    params = "/component/actions?group_by=component&start_date=" + start_date + "&end_date=" + end_date + "&order=asc"
     url     = base_url + file_key + params
     cursorurl = url
     headers = {'X-FIGMA-TOKEN' : figma_access_token}
     response = requests.get(url, headers=headers)
+    # print(url)
 
     output = pd.DataFrame()
 
@@ -125,11 +126,71 @@ def usages_by_file():
     output.to_csv("output/usages_by_file.csv", encoding='utf-8',  index=False)
 
 
+# VARIABLES
+def variable_actions_by_variable():
+    params = "/variable/actions?group_by=variable&start_date=" + start_date + "&end_date=" + end_date + "&order=asc"
+    url     = base_url + file_key + params
+    cursorurl = url
+    headers = {'X-FIGMA-TOKEN' : figma_access_token}
+    response = requests.get(url, headers=headers)
+    print(url)
+
+    output = pd.DataFrame()
+
+    # This API paginates at 1000 rows, looping over it if result is >999
+    while url:  
+        json_data = response.json()
+        normalisedRows = pd.json_normalize(json_data['rows'], meta=['week', 'variable_key', 'variable_name', 'detachments','insertions', 'collection_key', 'collection_name'])
+        output = pd.concat([output, normalisedRows])
+
+        # check for another page and get it; if not, exit the loop
+        if json_data['next_page']:
+            cursorurl = url + "&cursor=" + json_data['cursor']
+            print("Requesting next page: " + cursorurl)
+            response = requests.get(cursorurl, headers=headers)
+        else:
+            print("No new data. we're done")
+            url = ''
+
+    # Export to CSV
+    output.to_csv("output/variable_actions_by_variable.csv", encoding='utf-8',  index=False)
+
+def variable_actions_by_team():
+    params = "/variable/actions?group_by=team&start_date=" + start_date + "&end_date=" + end_date + "&order=asc"
+    url     = base_url + file_key + params
+    cursorurl = url
+    headers = {'X-FIGMA-TOKEN' : figma_access_token}
+    response = requests.get(url, headers=headers)
+    print(url)
+
+    output = pd.DataFrame()
+
+    # This API paginates at 1000 rows, looping over it if result is >999
+    while url:  
+        json_data = response.json()
+        normalisedRows = pd.json_normalize(json_data['rows'], meta=['week', 'variable_key', 'variable_name', 'detachments','insertions', 'collection_key', 'collection_name'])
+        output = pd.concat([output, normalisedRows])
+
+        # check for another page and get it; if not, exit the loop
+        if json_data['next_page']:
+            cursorurl = url + "&cursor=" + json_data['cursor']
+            print("Requesting next page: " + cursorurl)
+            response = requests.get(cursorurl, headers=headers)
+        else:
+            print("No new data. we're done")
+            url = ''
+
+    # Export to CSV
+    output.to_csv("output/variable_actions_by_team.csv", encoding='utf-8',  index=False)
+
+
 def main():
-    actions_by_component()
-    actions_by_team()
-    usages_by_component()
-    usages_by_file()
+    # actions_by_component()
+    # actions_by_team()
+    # usages_by_component()
+    # usages_by_file()
+    # variable_actions_by_variable()
+    variable_actions_by_team()
 
 
 if __name__ == "__main__":
